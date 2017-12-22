531f170cd9dff05990735b0b4a716d98cb7e87dd
Eduardo Cuomo <eduardo.cuomo@patagonian.it>
2017-12-22 16:52:18 -0300

    firefox

--- .src/firefox.sh
+++ .src/firefox.sh
@@ -8,6 +8,7 @@ sudo chown developer:developer $HOME_DEVELOPER_FF
 
 if [ ! -d "$FF_PROFILE" ]; then
   firefox -CreateProfile "Developer $FF_PROFILE" >/dev/null 2>&1 &
-else
-  firefox -new-instance -P "Developer" >/dev/null 2>&1 &
 fi
+sleep 1
+
+firefox -new-instance -P "Developer" >/dev/null 2>&1 &
