62e1528f7eabc17d5bf6c98228fa5e4435bce660
Eduardo Cuomo <eduardo.cuomo@patagonian.it>
2017-12-22 16:48:07 -0300

    firefox

--- .src/firefox.sh
+++ .src/firefox.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-FF_PROFILE='/home/developer/firefox/profile'
+HOME_DEVELOPER_FF='/home/developer/firefox'
+FF_PROFILE="$HOME_DEVELOPER_FF/profile"
+
+sudo chmod 777 $HOME_DEVELOPER_FF
+sudo chown developer:developer $HOME_DEVELOPER_FF
 
 if [ ! -d "$FF_PROFILE" ]; then
   firefox -CreateProfile "Developer $FF_PROFILE" >/dev/null 2>&1 &
