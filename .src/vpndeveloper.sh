7a3f69c1b76bbe67d87ae2f72f380ec9a9b7659d
Eduardo Cuomo <eduardo.cuomo@patagonian.it>
2018-01-03 12:48:59 -0300

    fix google chrome not wokring

--- .src/vpndeveloper.sh
+++ .src/vpndeveloper.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-HOME_DEVELOPER='/home/vpndeveloper'
-LOGS_FILE="$HOME_DEVELOPER/vpndeveloper-exec-$1.log"
+#https://github.com/jupyter/notebook/issues/2836
+export BROWSER=google-chrome
+
+export HOME_DEVELOPER='/home/vpndeveloper'
+export LOGS_FILE="$HOME_DEVELOPER/vpndeveloper-exec-$1.log"
 
 sudo chmod 777 $HOME_DEVELOPER
 sudo chown vpndeveloper:vpndeveloper $HOME_DEVELOPER
