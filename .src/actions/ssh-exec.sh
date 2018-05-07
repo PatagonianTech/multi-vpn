4ae2353a0a21733ecf564803a69248deb3a723a3
Eduardo Cuomo <reduardo7@gmail.com>
2018-05-07 14:41:48 -0300

    ssh-test & ssh-exec

---
+++ .src/actions/ssh-exec.sh
@@ -0,0 +1,48 @@
+## config_dir command*
+## Execute command on every configured server at VPN connected container.
+##
+## Params:
+##   config_dir: Configuration directory.
+##   command*:   Command to execute.
+
+. "${RESOURCES_PATH}/bootstrap.sh"
+local command="$@"
+
+if [ -f "$SSH_CONFIG_PATH" ]; then
+  configs=($(grep -E '^Host\s+[a-zA-Z0-9]' "$SSH_CONFIG_PATH" | sed -E 's/Host\s+//g'))
+
+  if [ -z "$configs" ]; then
+    @error "$SSH_CONFIG_PATH > Hosts not found"
+  else
+    ctotal=0
+    cok=0
+
+    for c in ${configs[@]}; do
+      ctotal=$((ctotal+1))
+
+      @print-line 120 '#'
+      @print "Progress: $ctotal of ${#configs[@]}..."
+
+      (
+        # Test
+        set -x
+        docker exec -it ${CONTAINER_NAME} ssh $c "$command"
+      ) && {
+        # Ok
+        cok=$((cok+1))
+        @print
+        @print 'OK'
+      } || {
+        # Error
+        @print
+        @print 'ERROR'
+      }
+    done
+
+    @print-line 120 '#'
+    @print
+    @print "Result: $cok connected of $ctotal"
+  fi
+else
+  @error "$SSH_CONFIG_PATH not defined"
+fi
