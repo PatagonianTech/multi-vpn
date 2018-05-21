d510ee6503a3820aa61225d1b0b5755566136923
Eduardo Cuomo <reduardo7@gmail.com>
2018-05-21 14:55:36 -0300

    ssh servers list sort

--- .src/utils/sshServersList.sh
+++ .src/utils/sshServersList.sh
@@ -2,4 +2,4 @@
 ## Get SSH Servers list.
 
 [ -f "$SSH_CONFIG_PATH" ] && \
-  grep -E '^Host\s+[a-zA-Z0-9]' "$SSH_CONFIG_PATH" | sed -E 's/Host\s+//g'
+  grep -E '^Host\s+[a-zA-Z0-9]' "$SSH_CONFIG_PATH" | sed -E 's/Host\s+//g' | sort -k9,9
